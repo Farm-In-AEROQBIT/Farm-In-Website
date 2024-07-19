b5f66a6f6ef979e8313fcc2f6ba845174d47bc8f
cheonsebin <lemon2842@naver.com>
2024-07-19 14:59:24 +0900

    Form.js 파일에 ID, Password 입력에 대한 이벤트 핸들러 추가, 로그인 버튼 작동 확인

--- main/src/components/Form.js
+++ main/src/components/Form.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Form.css';
 
 const Form = () => {
+
+    const [inputId, setInputId] = useState('');
+    const [inputPw, setInputPw] = useState('');
+
+    const handleInputId = (e) => {
+        setInputId(e.target.value);
+    }
+
+    const handleInputPw = (e) => {
+        setInputPw(e.target.value);
+    }
+
+    const onClickLogin = () => {
+        console.log('click login '+inputId+inputPw)
+    }
+
+
+
     return(
         <section>
             <div className='log-in-box'>
 
                 <div className='text-box-frame'>
                 <div className='icon-id'></div>
-                    <input type="text" className="input-id" placeholder='아이디'/>
+                    <input type="text" className="input-id" placeholder='아이디'
+                    value={inputId} onChange={handleInputId}/>
                 </div>
 
                 <div className='text-box-frame'>
                     <div className='icon-lock'></div>
-                    <input type="text" className='input-password' placeholder='비밀번호'/>
+                    <input type="text" className='input-password' placeholder='비밀번호'
+                    value={inputPw} onChange={handleInputPw}/>
                 </div>
 
-                <div className='log-in-btn'>
+                <div className='log-in-btn' onClick={onClickLogin}>
                     로그인
                 </div>
             </div>
@@ -24,3 +44,5 @@ const Form = () => {
     );
 };
 export default Form;
+
+
