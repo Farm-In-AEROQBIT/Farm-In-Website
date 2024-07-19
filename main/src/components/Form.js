a6a7e0fd18f3f8f0402019997278ba3ecaef70fb
cheonsebin <lemon2842@naver.com>
2024-07-19 17:10:27 +0900

    계정 만들기 텍스트를 버튼으로 수정

--- main/src/components/Form.js
+++ main/src/components/Form.js
@@ -15,9 +15,24 @@ const Form = () => {
     }
 
     const onClickLogin = () => {
-        console.log('click login '+inputId+inputPw)
+        console.log('click login '+inputId+' '+inputPw)
     }
 
+    const handleKeyDown =(e) => {
+        if(e.key == 'Enter'){
+            onClickLogin();
+        }
+    }
+
+
+    /*
+    useEffect(()=> {
+        axios.get('user_inform')
+        .then(res => console.log(res))
+        .catch()
+    }
+    ,[])
+    */
 
 
     return(
@@ -27,22 +42,20 @@ const Form = () => {
                 <div className='text-box-frame'>
                 <div className='icon-id'></div>
                     <input type="text" className="input-id" placeholder='아이디'
-                    value={inputId} onChange={handleInputId}/>
+                    value={inputId} onChange={handleInputId} onKeyDown={handleKeyDown}/>
                 </div>
 
                 <div className='text-box-frame'>
                     <div className='icon-lock'></div>
                     <input type="text" className='input-password' placeholder='비밀번호'
-                    value={inputPw} onChange={handleInputPw}/>
+                    value={inputPw} onChange={handleInputPw} onKeyDown={handleKeyDown}/>
                 </div>
 
                 <div className='log-in-btn' onClick={onClickLogin}>
                     로그인
                 </div>
-            </div>
+            </div> 
         </section>
     );
 };
 export default Form;
-
-
