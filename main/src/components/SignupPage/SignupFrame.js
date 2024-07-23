7848ca1c1576c14799a558b4ffced195a1d37ea5
cheonsebin <lemon2842@naver.com>
2024-07-23 11:45:07 +0900

    로고와 로그인 화면을 넣기 위한 프레임 구성

--- main/src/components/SignupPage/SignupFrame.js
+++ main/src/components/SignupPage/SignupFrame.js
@@ -5,10 +5,14 @@ const SignupFrame = () => {
     return(
     <main>
         <section className='sign-up-templete'>
-
+            <div className="information-frame">
+                가운데 프레임
+            </div>
+            <div className="logo-text">
+                로고 프레임
+            </div>
         </section>
     </main>
     );
 }
- 
 export default SignupFrame;
