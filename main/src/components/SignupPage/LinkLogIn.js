173d93c7dba0db3912bbd60da5ace03e8cafc686
cheonsebin <lemon2842@naver.com>
2024-07-24 17:58:06 +0900

    로그인 화면으로 돌아가기 링크를 텍스트에서 버튼으로 변환

---
+++ main/src/components/SignupPage/LinkLogIn.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './LinkLogIn.css';
+import {useNavigate} from 'react-router-dom';
+
+const LinkLigIn = () => {
+    return(
+        <main>
+            <p className="have-account">이미 팜인 계정을 가지고 있으신가요?</p>
+            <button className="turn-back-log-in">로그인 화면 돌아가기</button>
+        </main>
+
+    );
+};
+
+export default LinkLigIn;
+
--- main/src/router/AppRoutes.js
+++ main/src/router/AppRoutes.js
@@ -5,13 +5,14 @@ import SignupFrame from '../components/SignupPage/SignupFrame';
 import LinkSignIn from '../components/LoginPage/LinkSignIn';
 import Form from '../components/LoginPage/Form';
 import SignupForm from '../components/SignupPage/SignupForm';
+import LinkLogIn from '../components/SignupPage/LinkLogIn';
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<LogInFrame form={<Form />} link={<LinkSignIn />} />} />
             <Route path="/login" element={<LogInFrame />} />
-            <Route path="/signup" element={<SignupFrame form={<SignupForm/>}/>} />
+            <Route path="/signup" element={<SignupFrame form={<SignupForm/>} link={<LinkLogIn/>}/>} />
         </Routes>
     );
 };
