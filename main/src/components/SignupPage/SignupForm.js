c1e6d2c6350278c051b715b4f23919ade090cf2e
cheonsebin <lemon2842@naver.com>
2024-07-24 11:51:14 +0900

    비밀번호, 성명, 번화번호 입력란을 추가

--- main/src/components/SignupPage/SignupForm.js
+++ main/src/components/SignupPage/SignupForm.js
@@ -6,15 +6,19 @@ const InputField = ({ placeholder , icon, style}) => (
         <div className="icon-frame">
             <i className={icon}></i>
         </div>
-        <input type='text' className="regist-id" placeholder={placeholder} />
+        <input type='text' className="regist" placeholder={placeholder} />
     </fieldset>
 );
 
 const SignupForm = () => {
     return (
         <main>
-            <InputField placeholder="아이디" icon="icon-id" style={{top: 0, left: 0 }} />
-            <InputField placeholder="비밀번호" icon="icon-password" style={{top: 0, right: 0 }} />
+            <fieldset className="information">
+                <InputField placeholder="아이디" icon="icon-id" style={{top: 0, left: 0 }} />
+                <InputField placeholder="성명" icon="icon-name" style={{top: 0, right: 0 }} />
+                <InputField placeholder="비밀번호" icon="icon-password" style={{bottom: 0, left: 0 }} />
+                <InputField placeholder="전화번호" icon="icon-phone-number" style={{bottom: 0, right: 0 }} />
+            </fieldset>
         </main>
     );
 };
