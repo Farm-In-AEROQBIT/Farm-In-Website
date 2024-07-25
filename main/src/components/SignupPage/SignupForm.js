30be060bb73b7f8d40d31f090295ec2ce3163a73
cheonsebin <lemon2842@naver.com>
2024-07-25 11:34:19 +0900

    로그인 버튼에 기능 부여

--- main/src/components/SignupPage/SignupForm.js
+++ main/src/components/SignupPage/SignupForm.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import './SignupForm.css';
 
-const InputField = ({ placeholder , icon, style}) => (
-    <fieldset className="input-infotmation" style={style}>
-        <div className="icon-frame">
-            <i className={icon}></i>
-        </div>
-        <input type='text' className="regist" placeholder={placeholder} />
-    </fieldset>
-);
+// const handleKeyDown = (e) => {
+//     if(e.key === 'Enter'){
+
+//     }
+// }
+
+// const onClickAccount = () => {
+//     console.log(inputId)
+// }s
 
 const SignupForm = () => {
+
+    const [inputId, setInputId] =useState('');
+
+    const handleInput = (e) =>{
+        setInputId(e.target.value);
+    }
+
+    const onClickAccount = () => {
+        console.log("what is love?");
+    }
+
+
+    const InputField = ({ placeholder , icon, style}) => (
+
+        <fieldset className="input-infotmation" style={style}>
+            <div className="icon-frame">
+                <i className={icon}></i>
+            </div>
+            <input type='text' className="regist" placeholder={placeholder}/>
+        </fieldset>
+    );
+
+
+
     return (
         <main>
             <div className="create-account-form">
@@ -19,7 +44,7 @@ const SignupForm = () => {
                 <InputField placeholder="비밀번호" icon="icon-password" style={{bottom: 0, left: 0 }} />
                 <InputField placeholder="전화번호" icon="icon-phone-number" style={{bottom: 0, right: 0 }} />
             </div>
-            <button className="sign-in-btn">계정 생성</button>
+            <button className="sign-in-btn" onClick={onClickAccount}>계정 생성</button>
         </main>
     );
 };
