6b118f34dde8dc8e915321cc13858ae6bb5aba01
cheonsebin <lemon2842@naver.com>
2024-07-26 14:59:48 +0900

    중복확인 버튼 수정 / ID 생성 시 주의문 삽입

--- main/src/components/SignupPage/SignupForm.js
+++ main/src/components/SignupPage/SignupForm.js
@@ -2,16 +2,19 @@ import React, { useState } from "react";
 import './SignupForm.css';
 
 
-const InputField = ({ style, icon, placeholder, value, onChange, onKeyDown }) => (
-    <fieldset className="input-infotmation" style={style}>
-        <div className="icon-frame">
-            <i className={icon}></i>
-        </div>
-        <input type='text' className="regist" placeholder={placeholder} 
-            value={value} onChange={onChange} onKeyDown={onKeyDown}
-        />
-    </fieldset>
-);
+// const InputField = ({ style, icon, placeholder, value, onChange, onKeyDown }) => (
+//     <fieldset className="notice-frame" style={style}>
+//         <div className="input-infotmation">
+//             <div className="icon-frame">
+//                 <i className={icon}></i>
+//             </div>
+//             <input type='text' className="regist" placeholder={placeholder} 
+//                 value={value} onChange={onChange} onKeyDown={onKeyDown}/>
+//         </div>
+//     </fieldset>
+// );
+
+
 
 
 const SignupForm = () => {
@@ -50,17 +53,73 @@ const SignupForm = () => {
 
 
 
+    // return (
+    //     <main>
+    //         <div className="create-account-form">
+
+            
+
+    //             <InputField style={{top: 0, left: 0 }} icon="icon-id" placeholder="아이디" value={inputId} onChange={handleInputId} onKeyDown={handleKeyDown}/>
+    //             <InputField style={{top: 0, right: 0 }} icon="icon-password" placeholder="비밀번호" value={inputPw} onChange={handleInputPw} onKeyDown={handleKeyDown}/>
+    //             <InputField style={{bottom: 0, left: 0 }} icon="icon-name" placeholder="성명" value={inputName} onChange={handleInputName} onKeyDown={handleKeyDown}/>
+    //             <InputField style={{bottom: 0, right: 0 }} icon="icon-phone-number" placeholder="전화번호" value={inputNumber} onChange={handleInputNumber} onKeyDown={handleKeyDown}/>
+    //         </div>
+    //         <button className="sign-in-btn" onClick={onClickAccount}>계정 생성</button>
+    //     </main>
+    // );
+
+
     return (
         <main>
             <div className="create-account-form">
-                <InputField style={{top: 0, left: 0 }} icon="icon-id" placeholder="아이디" value={inputId} onChange={handleInputId} onKeyDown={handleKeyDown}/>
-                <InputField style={{top: 0, right: 0 }} icon="icon-password" placeholder="비밀번호" value={inputPw} onChange={handleInputPw} onKeyDown={handleKeyDown}/>
-                <InputField style={{bottom: 0, left: 0 }} icon="icon-name" placeholder="성명" value={inputName} onChange={handleInputName} onKeyDown={handleKeyDown}/>
-                <InputField style={{bottom: 0, right: 0 }} icon="icon-phone-number" placeholder="전화번호" value={inputNumber} onChange={handleInputNumber} onKeyDown={handleKeyDown}/>
+                <fieldset className="notice-frame" style={{top: 0, left:0}}>
+                    <div className="input-infotmation">
+                        <div className="icon-frame">
+                            <i className="icon-id"></i>
+                        </div>
+                        <input type='text' className="regist" placeholder="아이디" 
+                            value={inputId} onChange={handleInputId} onKeyDown={handleKeyDown}/>
+                        <button className="confirm-overlap-btn">중복확인</button>
+                    </div>
+                    <p className="notice-text">숫자와 영문자를 섞어서 8자 이상으로 만들어주세요</p>
+                </fieldset>
+
+                <fieldset className="notice-frame" style={{bottom: 0, left:0}}>
+                    <div className="input-infotmation">
+                        <div className="icon-frame">
+                            <i className="icon-password"></i>
+                        </div>
+                        <input type='text' className="regist" placeholder="비밀번호"
+                            value={inputPw} onChange={handleInputPw} onKeyDown={handleKeyDown}/>
+                    </div>
+                </fieldset>
+
+                <fieldset className="notice-frame" style={{top: 0, right:0}}>
+                    <div className="input-infotmation">
+                        <div className="icon-frame">
+                            <i className="icon-name"></i>
+                        </div>
+                        <input type='text' className="regist" placeholder="성명" 
+                            value={inputName} onChange={handleInputName} onKeyDown={handleKeyDown}/>
+                    </div>
+                </fieldset>
+
+                <fieldset className="notice-frame" style={{bottom: 0, right: 0}}>
+                    <div className="input-infotmation">
+                        <div className="icon-frame">
+                            <i className="icon-phone-number"></i>
+                        </div>
+                        <input type='text' className="regist" placeholder="전화 번호"
+                            value={inputNumber} onChange={handleInputNumber} onKeyDown={handleKeyDown}/>
+                    </div>
+                </fieldset>
+
             </div>
             <button className="sign-in-btn" onClick={onClickAccount}>계정 생성</button>
         </main>
     );
+
+
 };
 
 
