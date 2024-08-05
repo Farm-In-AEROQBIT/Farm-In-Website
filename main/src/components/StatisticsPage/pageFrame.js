c27a6616d1f23cd852c8a341d5ada14f4b3cf854
cheonsebin <lemon2842@naver.com>
2024-08-05 17:33:05 +0900

    useScreenSize의 js파일 생성/css 파일에 헤더 추가/

--- main/src/components/StatisticsPage/pageFrame.js
+++ main/src/components/StatisticsPage/pageFrame.js
@@ -1,12 +1,36 @@
 import React from "react";
+import { useState, useEffect } from 'react';
 import './PageFrame.css';
 
+// const useScreenSize = () => {
+//     const []
+// }
+
+
+
 const PageFrame = () => {
+
+    const [width, setWidth] = useState(window.innerWidth);
+
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWidth(window.innerWidth);
+        };
+    
+        // Resize 이벤트 리스너 등록
+        window.addEventListener('resize', handleResize);
+    
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return(
-        <main>
-            "다 잘 되게 해주세요"
+        <main className="backgrond" style = {{width: width}}>
+            <div className="header"></div>
         </main>
     );
-};
+}; 
 
 export default PageFrame;
