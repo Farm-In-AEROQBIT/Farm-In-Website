b4a7e9c76154ca62c10fb0016f8d87d9d8118467
cheonsebin <lemon2842@naver.com>
2024-08-05 14:24:18 +0900

    라우터에 통계 페이지 추가/component에 StatisticPage 폴더와 Frame 파일 생성

--- main/src/router/AppRoutes.js
+++ main/src/router/AppRoutes.js
@@ -6,12 +6,14 @@ import LinkSignIn from '../components/LoginPage/LinkSignIn';
 import Form from '../components/LoginPage/Form';
 import SignupForm from '../components/SignupPage/SignupForm';
 import LinkLogIn from '../components/SignupPage/LinkLogIn';
+import StatisticsFrame from '../components/StatisticsPage/pageFrame';
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<LogInFrame form={<Form />} link={<LinkSignIn />} />} />
             <Route path="/signup" element={<SignupFrame form={<SignupForm/>} link={<LinkLogIn/>}/>} />
+            <Route path="statistics" element={<StatisticsFrame/>}/>
         </Routes>
     );
 };
