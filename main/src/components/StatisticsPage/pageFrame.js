78fd772c822f8919b9d513331c89d5b962e36778
cheonsebin <lemon2842@naver.com>
2024-08-06 14:34:05 +0900

    기기의 모니터 크기 확인을 위해 useMaxScreenSize.js추가 / 확인을 위한 일시적인 화면 크기 출력 추가

--- main/src/components/StatisticsPage/pageFrame.js
+++ main/src/components/StatisticsPage/pageFrame.js
@@ -2,22 +2,26 @@ import React from "react";
 import { useState, useEffect } from 'react';
 import './PageFrame.css';
 import useScreenSize from "./useScreenSize";
-
+import useMaxScreenSize from "./useMaxScreenSize";
 
 
 const PageFrame = () => {
+    const screenSize = useScreenSize();
+    const maxScreenSize = useMaxScreenSize();
 
-    const screenSize = useScreenSize;
-
-    return(
-        // <main className="backgrond" style = {{width: screenSize.width}}>
+    return (
         <main>
-            <header className="header">
+            <header className="header" style = {{width: screenSize.width}}>
                 <svg className="company-logo"></svg>
             </header>
+            <h2 className="profile-bar" style = {{height: (screenSize.height)*0.5}}></h2>
+            <h1>Screen Size Detection with React Hook</h1>
+            <p>Width: {screenSize.width}</p>
+            <p>Height: {screenSize.height}</p>
+            <p>maxwidth: {maxScreenSize.width}</p>
+            <p>maxheight: {maxScreenSize.height}</p>
         </main>
     );
-}; 
- 
+};
+
 export default PageFrame;
- 
