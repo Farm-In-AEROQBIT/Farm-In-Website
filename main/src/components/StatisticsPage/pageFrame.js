78f21a8efe3734b2157a23434ce2b6a00959c47b
cheonsebin <lemon2842@naver.com>
2024-08-07 11:21:07 +0900

    로그아웃 버튼에 로그인 화면으로 돌아가는 기능을 추가

--- main/src/components/StatisticsPage/pageFrame.js
+++ main/src/components/StatisticsPage/pageFrame.js
@@ -3,19 +3,29 @@ import { useState, useEffect } from 'react';
 import './PageFrame.css';
 import useScreenSize from "./useScreenSize";
 import useMaxScreenSize from "./useMaxScreenSize";
+import {useNavigate} from 'react-router-dom';
 
 
 const PageFrame = () => {
     const screenSize = useScreenSize();
     const maxScreenSize = useMaxScreenSize();
+    const navigate = useNavigate();
+
+    const onClick = () => {
+        navigate("/");
+    }
 
     return (
-        <body>
-            <header className="header" style = {{width: screenSize.width, height: (maxScreenSize.height)*0.087}}>
+        <div>
+
+
+
+
+            <header className="header">
                 <svg className="company-logo"></svg>
             </header>
-            <h2 className="profile-bar" style = {{height: (maxScreenSize.height)*0.059}}>
-                <div className="buttons-frame" style = {{left: maxScreenSize.width*0.018, width: maxScreenSize.width*0.15}}>
+            <h2 className="profile-bar">
+                <div className="buttons-frame" onClick={onClick} style = {{left: maxScreenSize.width*0.018}}>
                     <button className="my-information-btn"> 내 정보 보기 </button>
                     <button className="log-out-btn"> 로그아웃</button>
                 </div>
@@ -23,16 +33,19 @@ const PageFrame = () => {
                     <div className="image-circle"></div>
                 </div>
             </h2>
-            <section>
-                <image></image>
-            </section>
-            <h1>Screen Size Detection with React Hook</h1>
+            {/* <section className="image-container" style ={{width: (maxScreenSize.width)*0.65, height: (maxScreenSize.height)*0.55}}>
+                <image className="header-image"></image>
+
+                <image className="overlay"></image>
+            </section> */}
+
+            {/* <h1>Screen Size Detection with React Hook</h1>
             <p>Width: {screenSize.width}</p>
             <p>Height: {screenSize.height}</p>
             <p>maxwidth: {maxScreenSize.width}</p>
-            <p>maxheight: {maxScreenSize.height}</p>
+            <p>maxheight: {maxScreenSize.height}</p> */}
 
-        </body>
+        </div>
     );
 };
 
