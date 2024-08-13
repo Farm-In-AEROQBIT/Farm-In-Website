0dcbe6b17ed5ce40acb593e0257fb614f52e6f8d
cheonsebin <lemon2842@naver.com>
2024-08-13 14:37:21 +0900

    조회버튼과 csv export 버튼 추가

--- main/src/components/StatisticsPage/StatisticGraph.js
+++ main/src/components/StatisticsPage/StatisticGraph.js
@@ -7,14 +7,19 @@ const StatisticGraph = () => {
     return(
         <main className="graph-container" ref={ref}>
             <summary style={{margin: 0}} className="title-box">
-                <span className="graph-title-bold" style={{fontSize: (bodySize.width/25)}}>실시간&nbsp;</span>
-                <span className="graph-title" style={{fontSize: (bodySize.width/25)}}>업데이트<br/></span>
-                <span className="graph-title" style={{fontSize: (bodySize.width/25)}}>농장&nbsp;</span>
-                <span className="graph-title-bold" style={{fontSize: (bodySize.width/25)}}>공기 정보&nbsp;</span>
-                <span className="graph-title" style={{fontSize: (bodySize.width/25)}}>확인하기</span>
+                <span className="Nanum-Gothic-bold" style={{fontSize: (bodySize.width/25)}}>실시간&nbsp;</span>
+                <span className="Nanum-Gothic-light" style={{fontSize: (bodySize.width/25)}}>업데이트<br/></span>
+                <span className="Nanum-Gothic-light" style={{fontSize: (bodySize.width/25)}}>농장&nbsp;</span>
+                <span className="Nanum-Gothic-bold" style={{fontSize: (bodySize.width/25)}}>공기 정보&nbsp;</span>
+                <span className="Nanum-Gothic-light" style={{fontSize: (bodySize.width/25)}}>확인하기</span>
             </summary>
             <div className='display-section'>
-                <div className='grid-test'>1</div>
+                <div className='grid-test'></div>
+                <div className='btn-section'>
+                    <button className='inquiry-btn'>
+                    </button>
+                    <button className='export-btn'></button>
+                </div>
             </div> 
             {/* <hr className='bottom-hr'/> */}
         </main>
